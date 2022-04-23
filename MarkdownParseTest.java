949a7af9ec33b1bd77f19f1a66bd09349848daa0
ElvisJoa <elvisjoa@gmail.com>
2022-04-22 18:46:36 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -15,5 +15,8 @@ public class MarkdownParseTest {
     @Test
     public void checkLinks() throws IOException{
         assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
+        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("parenthesis-test.md"))));
+        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("brackets-test.md"))));
+        assertEquals(List.of("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/syllabus","canvas.ucsd.edu"), MarkdownParse.getLinks(Files.readString(Path.of("another-test.md"))));
     }
 }
