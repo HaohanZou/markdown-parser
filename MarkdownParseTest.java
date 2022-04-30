309519da94bb2a5f53ef2bd25ffb3c61d4ccf5ee
ElvisJoa <elvisjoa@gmail.com>
2022-04-29 18:39:33 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,14 +14,14 @@ public class MarkdownParseTest{
 
     @Test
     public void checkAllLinks() throws IOException{
-        Path file1 = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/test-file.md");
-        Path file2 = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/test-file2.md");
-        Path file3 = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/test-file3.md");
-        Path file4 = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/test-file4.md");
-        Path file5 = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/test-file5.md");
-        Path file6 = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/test-file6.md");
-        Path file7 = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/test-file7.md");
-        Path file8 = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/test-file8.md");
+        Path file1 = Path.of("test-file.md");
+        Path file2 = Path.of("test-file2.md");
+        Path file3 = Path.of("test-file3.md");
+        Path file4 = Path.of("test-file4.md");
+        Path file5 = Path.of("test-file5.md");
+        Path file6 = Path.of("test-file6.md");
+        Path file7 = Path.of("test-file7.md");
+        Path file8 = Path.of("test-file8.md");
 
         System.out.println("test-file.md: " + MarkdownParse.getLinks(Files.readString(file1)));
         System.out.println("test-file2.md: " + MarkdownParse.getLinks(Files.readString(file2)));
@@ -45,24 +45,19 @@ public class MarkdownParseTest{
 
     @Test
     public void checkLinks1() throws IOException{
-        Path path = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/test-file.md");
+        Path path = Path.of("test-file.md");
         List<String> links = MarkdownParse.getLinks(Files.readString(path));
         assertEquals(List.of("https://something.com", "some-thing.html"), links);
     }
 
     @Test
     public void checkLinks2() throws IOException{
-        Path path = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/parenthesis-test.md");
+        Path path = Path.of("parenthesis-test.md");
         assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(path)));
     }
     @Test
     public void checkLinks3() throws IOException{
-        Path path = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/brackets-test.md");
-        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(path)));
-    }
-    @Test
-    public void checkLinks4() throws IOException{
-        Path path = Path.of("C:/Users/Elvis Joa/OneDrive/Documents/GitHub/markdown-parser/brackets-test.md");
+        Path path = Path.of("brackets-test.md");
         assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(path)));
     }
 }
